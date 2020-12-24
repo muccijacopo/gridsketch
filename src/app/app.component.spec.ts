3cd8f1d51eebeb93184bcffcec07e56cf3574343
Jacopo Mucci <mucci.jacopo@gmail.com>
2020-12-24 18:15:28 +0100

    changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myroom'`, () => {
+  it(`should have as title 'websketch'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myroom');
+    expect(app.title).toEqual('websketch');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myroom app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('websketch app is running!');
   });
 });
