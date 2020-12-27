1f81934d771793b60fe2b34d3b303824a4da4628
Jacopo Mucci <mucci.jacopo@gmail.com>
2020-12-27 23:08:03 +0100

    modified project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('websketch app is running!');
+    expect(await page.getTitleText()).toEqual('gridsketch app is running!');
   });
 
   afterEach(async () => {
